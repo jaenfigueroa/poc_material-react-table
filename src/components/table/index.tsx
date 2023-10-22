089da93e0750ffc4736db0d736f27bbb26fe8865
Aldair Chuman <aldair@DESKTOP-T7OTJRV.localdomain>
2023-10-21 20:58:10 -0500

    feat: create files for data and interface Rule

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,74 +1,47 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { MaterialReactTable } from 'material-react-table';
 import { MRT_ColumnDef } from 'material-react-table';
+import { Rule } from './IRule'; // Importa la interfaz Rule desde el archivo Rule.ts
+import { data } from './dataRule'; // Importa la data desde el archivo data.ts
 
-interface Rule {
-  codigo: string;
-  regla: string;
-  tipo: string;
-  severidad: string;
-  e_actual: string;
-  fecha: string;
-}
+export default function Table() {
+    const columns = useMemo<MRT_ColumnDef<Rule>[]>(
+        () => [
+            {
+                accessorKey: 'codigo',
+                header: 'Codigo',
+            },
+            {
+                accessorKey: 'regla',
+                header: 'Regla',
+            },
+            {
+                accessorKey: 'tipo',
+                header: 'Tipo',
+            },
+            {
+                accessorKey: 'severidad',
+                header: 'Severidad',
+            },
+            {
+                accessorKey: 'e_actual',
+                header: 'E.Actual',
+            },
+            {
+                accessorKey: 'fecha',
+                header: 'Fecha',
+            },
+        ],
+        [],
+    );
 
-const data: Rule[] = [
-  {
-    codigo: 'R01',
-    regla: 'Regla 1',
-    tipo: 'Bug',
-    severidad: 'Major',
-    e_actual: 'activado',
-    fecha: '20-10-2023',
-  },
-  {
-    codigo: 'R02',
-    regla: 'Regla 2',
-    tipo: 'Vulnerability',
-    severidad: 'Critical',
-    e_actual: 'desactivado',
-    fecha: '08-05-2023',
-  },
-];
-
-export default function App() {
-  const columns = useMemo<MRT_ColumnDef<Rule>[]>(
-    () => [
-      {
-        accessorKey: 'codigo',
-        header: 'Codigo',
-      },
-      {
-        accessorKey: 'regla',
-        header: 'Regla',
-      },
-      {
-        accessorKey: 'tipo',
-        header: 'Tipo',
-      },
-      {
-        accessorKey: 'severidad',
-        header: 'Severidad',
-      },
-      {
-        accessorKey: 'e_actual',
-        header: 'E.Actual',
-      },
-      {
-        accessorKey: 'fecha',
-        header: 'Fecha',
-      },
-
-    ],
-    [],
-  );
-
-  return (
-    <MaterialReactTable
-      columns={columns}
-      data={data}
-      enableRowSelection
-      enableColumnOrdering
-      enableGlobalFilter
-    />
-  );
+    return (
+        <MaterialReactTable
+            columns={columns}
+            data={data}
+            enableRowSelection
+            enableColumnOrdering
+            enableGlobalFilter
+        />
+    );
 }
