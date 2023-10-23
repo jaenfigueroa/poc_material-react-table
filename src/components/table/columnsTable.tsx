2ec9a95856a8b42ea31f42873fb014ccaf50e59a
Aldair Chuman <aldair@DESKTOP-T7OTJRV.localdomain>
2023-10-23 09:51:43 -0500

    fix: bug

--- src/components/table/columnsTable.tsx
+++ src/components/table/columnsTable.tsx
@@ -1,34 +1,26 @@
-import { useMemo } from 'react';
-import { MRT_ColumnDef } from 'material-react-table';
-import { Rule } from './IRule';
-
-const generateColumns = (): MRT_ColumnDef<Rule>[] => {
-    return [
-        {
-            accessorKey: 'codigo',
-            header: 'Codigo',
-        },
-        {
-            accessorKey: 'regla',
-            header: 'Regla',
-        },
-        {
-            accessorKey: 'tipo',
-            header: 'Tipo',
-        },
-        {
-            accessorKey: 'severidad',
-            header: 'Severidad',
-        },
-        {
-            accessorKey: 'e_actual',
-            header: 'E.Actual',
-        },
-        {
-            accessorKey: 'fecha',
-            header: 'Fecha',
-        },
-    ];
-};
-
-export const columns = useMemo(() => generateColumns(), []);
+export const columns: any[] = [
+    {
+        accessorKey: 'codigo',
+        header: 'Codigo',
+    },
+    {
+        accessorKey: 'regla',
+        header: 'Regla',
+    },
+    {
+        accessorKey: 'tipo',
+        header: 'Tipo',
+    },
+    {
+        accessorKey: 'severidad',
+        header: 'Severidad',
+    },
+    {
+        accessorKey: 'e_actual',
+        header: 'E.Actual',
+    },
+    {
+        accessorKey: 'fecha',
+        header: 'Fecha',
+    },
+];
